455dd879f1d53794821568e0d847b96ee25f4164
Nikita Shendelyov <nshendelev@mail.ru>
2020-10-28 10:56:06 +0700

    Урок 6

---
+++ src/com/company/Matmain.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+
+public class Matmain {
+
+    public static void main(String[] args) {
+
+        Scanner num = new Scanner(System.in);
+        int first, second, result;  //при делении лучше использовать тип даннх типа float
+        System.out.print("Введи первое число: ");
+        first = num.nextInt(); // если float то меняем на num.nextFloat()
+        System.out.print("Введи второе число: ");
+        second = num.nextInt();
+        result = first + second; //можно "-" "*" "/" при делении лучше использовать тип даннх типа float
+        System.out.println("Сумма введенных чисел равна: " + result);
+        result += 2; //сокращенная запись также и при "-" "*" "/"
+        result++; //сокращенная запись прибавления еденицы также и при "-"
+
+        int result1 = first % second; //% выводит остаток при делении
+        System.out.println("Остаток при делении равен: " + result1);
+
+    }
+}
