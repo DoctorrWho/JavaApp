1a1ce5ebc80a41cabb74e2783a5a10e031f4ead3
Nikita Shendelyov <nshendelev@mail.ru>
2020-10-29 16:43:28 +0700

    Урок номер 8 Цыклы (For, While, Do while)

--- src/com/company/Cikl.java
+++ src/com/company/Cikl.java
@@ -23,9 +23,21 @@ public class Cikl {
         int x = 0;
         System.out.println ("\n");
         do{
-            System.out.println(x);
+            System.out.println(x + " Вывелось один раз при не соответствующем условии");
             x++;
         }while(x < 0);
+
+        //также можно в цыклах использовать операторы "break" "return" - закончат цикл и выйдет из него
+        // "continue" - пропускает одну итерацию в цыкле
+
+        System.out.println ("\n");
+
+        int q = 0;
+        while(q < 50){
+            q += 1;
+            if (q % 2 == 0){continue;}
+            System.out.println (q);
+        }
     }
 
 }
