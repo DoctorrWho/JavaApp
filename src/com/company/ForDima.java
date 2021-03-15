3a86b18b4ef45133398fb2e0680f98db27f25f80
DoctorWho <nshendelev@mail.ru>
2021-03-15 17:55:49 +0700

    Projekt For Dima

--- src/com/company/ForDima.java
+++ src/com/company/ForDima.java
@@ -3,7 +3,17 @@ package com.company;
 public class ForDima {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Projekt For Dima!\n");
+        int Stroka = 5;
+        for(int i = 0; i < Stroka; i++){
+                for(int j = 0; j < i; j++){
+                        for(int k = j; k < 1; k++){
+                            System.out.print("X");
+                        }
+                System.out.print(" ");
+                }
+        System.out.println("X");
+        }
 
     }
 }
